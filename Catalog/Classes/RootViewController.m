19617ea70c9dd37ae7ea5307085da33b2de7c5b8
Andy Waite <andy@huddletech.com>
2010-04-14 19:28:39 +0100

    use correct table style for LongTextFieldViewController

--- Catalog/Classes/RootViewController.m
+++ Catalog/Classes/RootViewController.m
@@ -130,7 +130,7 @@
 		[self.navigationController pushViewController:controller animated:YES];
 		[controller release];
 	} else if ([selectedItemLabel isEqualToString:@"LongTextFieldViewController"]){
-		LongTextFieldViewController *controller = [[LongTextFieldViewController alloc] init];
+		LongTextFieldViewController *controller = [[LongTextFieldViewController alloc] initWithStyle:UITableViewStyleGrouped];
 		controller.delegate = self;
 		controller.string = @"Pre-populated text";
 		[self.navigationController pushViewController:controller animated:YES];
