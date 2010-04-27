501aa95f52b91d1bb41645552d9434bba2d20cf5
Andy Waite <andy@huddletech.com>
2010-04-28 00:58:28 +0100

    whitespace

--- Catalog/Classes/AlertView.m
+++ Catalog/Classes/AlertView.m
@@ -5,10 +5,7 @@
 
 -(void)viewDidLoad{
 	[self showActivityAlert:@"Stuff happening" message:@"Please wait 3 seconds..."];
-	
 	[NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(timerFinished) userInfo:nil repeats:NO];
-
-
 }
 
 -(void)timerFinished{
